e045e491565161023f1d89ff8680714a31c0af5e
vapor05 <nicholasbocchini@gmail.com>
2024-07-07 17:32:37 -0500

    updated code to use iterators

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ use std::env;
 use std::process;
 use rinigrep::Config;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    let config = Config::build(&args).unwrap_or_else(|err| {
+fn main() {   
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
